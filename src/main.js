9c073ee078dcb51969404272dc76c11fd48a3192
Balázs Váradi <netpok@gmail.com>
2024-11-27 15:12:01 +0100

    New example

--- src/main.js
+++ src/main.js
@@ -1,25 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import paperFull from 'paper'
+const withSourceMap = 'content-with-sourcemap'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite! ${paperFull.Color.length}</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const regex = new RegExp("\n//# sourceMappingUrl=")
 
-setupCounter(document.querySelector('#counter'))
+const withoutSourceMap=withSourceMap.replace(regex, '');
+
+document.querySelector('#app').innerHTML = withoutSourceMap
